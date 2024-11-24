ea7cbc58d494e187a3843a6dfa304616680e20ff
Hendy Nur Sholeh <102641376+HendyNurSholeh@users.noreply.github.com>
2024-11-24 13:50:09 +0800

    leaflet: implement rute and rute awal and destination

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -11,7 +11,8 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="node_modules/leaflet/dist/leaflet.css" />
     <script src="node_modules/leaflet/dist/leaflet.js"></script>
-    <!-- STYLES -->
+    <link rel="stylesheet" href="node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+    <script src="node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
 
 </head>
 
@@ -25,17 +26,46 @@
         </div>
     </div>
 
-    <script {csp-script-nonce}>
+    <script>
     document.addEventListener('DOMContentLoaded', function() {
-        var map = L.map('map').setView([-3.316694, 114.590111], 13);
+        // Inisialisasi peta dan atur tampilan ke batas yang ditentukan
+        var map = L.map('map').fitBounds([
+            [-3.316694, 114.590111], // Banjarmasin
+            [-3.80044, 115.20482] // Satui
+        ], {
+            padding: [50, 50]
+        });
 
+        // Tambahkan ubin OpenStreetMap ke peta
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> kontributor'
         }).addTo(map);
 
-        L.marker([-3.316694, 114.590111]).addTo(map)
-            .bindPopup('Lokasi Saya')
-            .openPopup();
+        // Tambahkan penanda untuk Banjarmasin dan Satui
+        var bjm = L.marker([-3.316694, 114.590111]).addTo(map).bindPopup('Banjarmasin<br>CKB Banjarmasin');
+        var satui = L.marker([-3.80044, 115.20482]).addTo(map).bindPopup('Tujuan:<br>CKB Satui');
+
+        // Tambahkan kontrol rute ke peta
+        L.Routing.control({
+            waypoints: [
+                L.latLng(-3.316694, 114.590111), // Banjarmasin (mulai)
+                L.latLng(-3.80044, 115.20482) // Satui (tujuan)
+            ],
+            routeWhileDragging: true
+        }).addTo(map);
+
+        // Buka popup untuk kedua penanda
+        bjm.openPopup();
+        satui.openPopup();
+
+        // Sesuaikan tampilan peta untuk memastikan kedua penanda dan rute terlihat
+        map.fitBounds([
+            [-3.316694, 114.590111], // Banjarmasin
+            [-3.80044, 115.20482] // Satui
+        ], {
+            padding: [50, 50],
+            maxZoom: 10 // Atur tingkat zoom maksimum untuk memastikan rute terlihat
+        });
     });
     </script>
 </body>
